5373561cedd99c3cbf729550430bc583aa538de8
Abiu12 <callejas195@gmail.com>
2025-05-12 12:22:26 -0600

    se modifica el texto

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,7 +7,9 @@ import { Component } from '@angular/core';
   standalone: false,
 })
 export class HomePage {
-
+  text: string = "Primero"
   constructor() {}
-
+  cambarTexto() {
+    this.text = "Segundo"
+  }
 }
